37c108438b32603ccd824a9cb51633007904e1d6
sidpatki <siddharth.patki@ltimindtree.com>
2024-02-25 08:55:53 +0530

    Add

--- src/test/java/IRCTGIT.java
+++ src/test/java/IRCTGIT.java
@@ -3,12 +3,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class IRCTGIT {
 	public static void main(String[] args) {
-		
 
 		System.out.println("devlop brnach");
-
-
 		System.out.println("This is BETA Branch ");
+		System.out.println("Kya hai ki ");
+		System.out.println("uda maaaka ");
 }
 }
 
