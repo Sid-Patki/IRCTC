cae332b3c44143fce111d1732426b6b2336f627f
sidpatki <siddharth.patki@ltimindtree.com>
2024-02-25 08:53:38 +0530

    kya hai ki

--- src/test/java/IRCTGIT.java
+++ src/test/java/IRCTGIT.java
@@ -4,7 +4,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class IRCTGIT {
 	public static void main(String[] args) {
 		
-		System.out.println("devlop brnach");
-	}
 
+		System.out.println("devlop brnach");
+
+
+		System.out.println("This is BETA Branch ");
 }
+}
+
