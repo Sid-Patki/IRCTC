3a9c9233a47e7d95c8137a0ddc5a3951cd3fa11a
sidpatki <siddharth.patki@ltimindtree.com>
2024-02-25 09:18:48 +0530

    this golden upload

--- src/test/java/IRCTGIT.java
+++ src/test/java/IRCTGIT.java
@@ -4,14 +4,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class IRCTGIT {
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
-		System.out.println("devlop brnach");
-	}
 
-}
-=======
+		System.out.println("devlop brnach");
 		System.out.println("This is BETA Branch ");
 		System.out.println("ADD things from BETA");
 }
 }
->>>>>>> Betairctc
+
